7072b12d26e08cdd7d5669336d32ec141ca4256e
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2022-09-21 01:45:39 -0400

    Completed Selenium Python Program

--- seleniumsession/WebDriverBasics.py
+++ seleniumsession/WebDriverBasics.py
@@ -1,4 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome(executable_path="C:\\Users\shiva\\eclipse-workspace\\Myproject\\chromedriver.exe")
 driver.get("http://www.google.com")
 print(driver.title)
+
+
+driver.find_element(By.NAME,"q").send_keys('Python Frameworks lists')
+driver.implicitly_wait(5)
+list = driver.find_elements(By.CSS_SELECTOR,'ul.erkvQe li span')
+print(len(list))
+
+for elementlist in list:
+    print(elementlist.text)
+    if elementlist.text == "python framework list for automation":
+        elementlist.click()
+        break
+
+driver.quit()
+
+
