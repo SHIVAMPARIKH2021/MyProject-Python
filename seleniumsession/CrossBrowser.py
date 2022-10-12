4d5477c526f13a75de45bc9f00dec911dece6126
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2022-10-12 00:16:38 -0400

    WebDriverManager is used to run the script

---
+++ seleniumsession/CrossBrowser.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+browser_name = "chrome"
+if browser_name == "chrome":
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+elif browser_name == "edge":
+    driver = webdriver.Edge(EdgeChromiumDriverManager().instaLL())
+elif browser_name == "firefox":
+    driver = webdriver.Firefox(service=GeckoDriverManager().install())
+else:
+    print("please pass the browser_name: "+browser_name)
+
+driver.implicitly_wait(5)
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.maximize_window()
+driver.find_element(By.NAME,"username").send_keys("Admin")
+driver.find_element(By.NAME,"password").send_keys("admin123")
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
+page_title = driver.title
+print(str(page_title))
+
+driver.quit()
